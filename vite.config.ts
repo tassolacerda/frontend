21dcb4880f5397a04f3e304cecb9a5bce9ab9d82
Paulo Lacerda <paulooliveiralacerda@gmail.com>
2025-10-20 10:20:25 -0300

    fix: fix port

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
